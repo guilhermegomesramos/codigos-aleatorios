dd0b2fc40a20647f60a5ca0d73b7cdcc88c5c162
guilhermegomesramos <168496717+guilhermegomesramos@users.noreply.github.com>
2025-03-10 21:28:47 -0300

    0.1

---
+++ codigosAleatoriosDeSala/calculadoraFuncoes.java
@@ -0,0 +1,32 @@
+public class calculadoraFuncoes {
+
+    double primeiroValor;
+    double segundoValor;
+    double resultado;
+    
+    double somar(double valor1, double valor2 ){
+
+        primeiroValor = valor1;
+        segundoValor = valor2;
+        resultado = primeiroValor + segundoValor;
+        return resultado;
+    }
+    double subtrair(double valor1, double valor2){
+        primeiroValor = valor1;
+        segundoValor = valor2;
+        resultado = primeiroValor + segundoValor;
+        return resultado;
+    }
+    double mutiplicar(double valor1, double valor2){
+        primeiroValor = valor1;
+        segundoValor = valor2;
+        resultado = primeiroValor + segundoValor;
+        return resultado; 
+    }
+    double dividir(double valor1, double valor2){
+        primeiroValor = valor1;
+        segundoValor = valor2;
+        resultado = primeiroValor + segundoValor; 
+        return resultado;
+    }
+}
---
+++ codigosAleatoriosDeSala/rodandoCalculadora.java
@@ -0,0 +1,5 @@
+public class rodandoCalculadora {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
